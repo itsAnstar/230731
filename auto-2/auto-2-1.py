2e53085a5820617d93867f939424d1f642f1235d
Macin2 <Lucky_Nigel@yeah.net>
2023-08-06 21:44:19 +0800

    Update auto-2-1.py

--- auto-2/auto-2-1.py
+++ auto-2/auto-2-1.py
@@ -1,13 +1,21 @@
 import tkinter as tk
 
+
 def on_button1_click():
     print("开始任务被点击")
+
 
 def on_button2_click():
     print("停止任务被点击")
 
+
 window = tk.Tk()
 window.title("直链提取进程")
+# 设置窗体大小
+window.minsize(275, 100)
+
+# 锁定窗体大小，使其不可缩放
+window.resizable(False, False)
 
 button1 = tk.Button(window, text="开始", command=on_button1_click)
 button1.pack(padx=20, pady=10)
